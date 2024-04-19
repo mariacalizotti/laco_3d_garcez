5ab208411b17720ef56ceb5df3077f2d74084355
mariacalizotti <108734342+mariacalizotti@users.noreply.github.com>
2024-04-19 09:54:51 -0300

    altmaria

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ function verificarTime() {
     var respostaTime = prompt("Qual os melhor time?");
 
     
-    if (respostaTime.toLowerCase() === "Athlético PR") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "Luan Santana") {
+      alert("Isso mesmo! O Luan santana é o melhor cantor!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
       alert("Não é isso! Tente novamente.");
